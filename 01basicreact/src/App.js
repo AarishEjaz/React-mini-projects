e5c93ca162d8e21ac835a1993e2bde8a3b928819
Aarish Ejaz <75199171+AarishEjaz@users.noreply.github.com>
2025-02-05 23:07:17 +0530

    added some code

---
+++ 01basicreact/src/App.js
@@ -0,0 +1,9 @@
+
+
+function App() {
+  return (
+    <h1>programmingBeast</h1>
+  );
+}
+
+export default App;
---
+++ vite-react/src/App.jsx
@@ -0,0 +1,10 @@
+
+function App() {
+
+
+  return (
+    <h1>programmingBeast</h1>
+  )
+}
+
+export default App
