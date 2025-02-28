628fef90f1c174419a70295db6cbbbc681d02c77
Aarish Ejaz <75199171+AarishEjaz@users.noreply.github.com>
2025-02-28 23:34:55 +0530

    added some components

--- react-router/src/main.jsx
+++ react-router/src/main.jsx
@@ -2,11 +2,14 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import { createBrowserRouter, Route, RouterProvider,createRoutesFromElements } from 'react-router-dom'
 import Layout from './Layout.jsx'
 import Home from './components/Home/Home.jsx'
 import About from './components/about/About.jsx'
+import Contact from './components/contact/Contact.jsx'
 
+
+//One way of creating router
 const router = createBrowserRouter([
   {
     path: '/',
@@ -19,11 +22,26 @@ const router = createBrowserRouter([
       {
         path: "about",
         element: <About />
-      },{}
+      },
+      {
+        path: "contact",
+        element:<Contact />
+      }
     ]
 
   }
 ])
+
+// Another way of creating router
+// const router = createBrowserRouter(
+//   createRoutesFromElements(
+//     <Route path="/" element={<Layout />}>
+//       <Route path="" element={<Home />} />  {/* index = path: "" */}
+//       <Route path="about" element={<About />} />
+//       <Route path="contact" element={<Contact />} />
+//     </Route>
+//   )
+// );
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
