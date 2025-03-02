7d930a5ddcb0e9c12fa922b129ec6d1b3cdd503a
Aarish Ejaz <75199171+AarishEjaz@users.noreply.github.com>
2025-03-03 00:56:21 +0530

    added some code

--- react-router/src/components/User/User.jsx
+++ react-router/src/components/User/User.jsx
@@ -1,9 +1,11 @@
-import React from 'react'
+import React, { Component } from 'react'
+import { useParams } from 'react-router-dom'
 
-function User() {
-  return (
-    <div>User</div>
-  )
+function  User(){
+    const {userid} = useParams()
+    return(
+        <div className='bg-gray-600 text-white text-3xl p-4'>User: {userid} </div>
+    )
 }
 
 export default User
