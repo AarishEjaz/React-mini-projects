1403ad24787715ecbcd746e2a683a3235179a173
Aarish Ejaz <75199171+AarishEjaz@users.noreply.github.com>
2025-03-26 00:44:42 +0530

    completed context

--- todo-context-local/src/App.jsx
+++ todo-context-local/src/App.jsx
@@ -8,8 +8,12 @@ function App() {
 
   const[todos,setTodos] = useState([])
 
+  const addTodo = (todo) =>{
+    setTodos((prev)=>[...prev])
+  }
+
   return (
-    <TodoProvider>
+    <TodoProvider value={{todos, addTodo, updateTodo, deleteTodo, toggleComplete}}>
       <div>programming Beast</div>
       <div className="bg-[#172842] min-h-screen py-8">
                 <div className="w-full max-w-2xl mx-auto shadow-md rounded-lg px-4 py-3 text-white">
