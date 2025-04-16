71531f822b2250f95e588e8ee4381661d7a8e714
Aarish Ejaz <75199171+AarishEjaz@users.noreply.github.com>
2025-04-16 19:55:30 +0530

    updated auth service

--- MegaBlog/src/appwrite/auth.js
+++ MegaBlog/src/appwrite/auth.js
@@ -1,6 +1,18 @@
 import conf from '../conf/conf.js'
+import {Client, Account, ID} from "appwrite"
 
-export class AuthService{}
+export class AuthService{
+    client = new Client()
+    account
+
+    constructor(){
+        this.client
+        .setEndpoint(conf.appwriteUrl)
+        .setProject(conf.appwriteProjectId)
+        this.account = new Account(this.client)
+    }
+
+}
 
 const autheService = new AuthService()
 
