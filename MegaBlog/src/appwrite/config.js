14ce28d9ee8130b0b5e8ef6a0d80fa971c6a1b16
Aarish Ejaz <75199171+AarishEjaz@users.noreply.github.com>
2025-04-26 19:59:41 +0530

    added some changes

--- MegaBlog/src/appwrite/config.js
+++ MegaBlog/src/appwrite/config.js
@@ -1,3 +1,4 @@
+import { stat } from "fs";
 import conf from "../conf/conf.js";
 import { Client, Account, ID, Databases, Storage, Query } from "appwrite";
 
@@ -11,6 +12,25 @@ export class Service{
         .setProject(conf.appwriteProjectId)
         this.databases  =  new Databases(this.client)
         this.bucket = new Storage(this.client)
+    }
+
+    async createPost({title, slug, content, featuredImage, status, userId}){
+        try{
+            return await this.databases.createDocument(
+                conf.appwriteDatabaseId
+                conf.appwriteCollectionId
+                slug,
+                {
+                    title,
+                    content,
+                    featuredImage,
+                    status,
+                    userId,
+                }
+            )
+        }catch(error){
+            console.log(error)
+        }
     }
 
 }
