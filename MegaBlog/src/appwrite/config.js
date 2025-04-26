db88d520b53f683e0473500f2774602afc799bab
Aarish Ejaz <75199171+AarishEjaz@users.noreply.github.com>
2025-04-26 19:46:06 +0530

    added some code

--- MegaBlog/src/appwrite/config.js
+++ MegaBlog/src/appwrite/config.js
@@ -9,6 +9,8 @@ export class Service{
         this.client
         .setEndpoint(conf.appwriteUrl)
         .setProject(conf.appwriteProjectId)
+        this.databases  =  new Databases(this.client)
+        this.bucket = new Storage(this.client)
     }
 
 }
