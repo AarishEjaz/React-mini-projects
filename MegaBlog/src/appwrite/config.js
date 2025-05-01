a9920482b69878c716c250a2f6987d4064b90e74
Aarish Ejaz <75199171+AarishEjaz@users.noreply.github.com>
2025-05-01 11:22:48 +0530

    added controllers

--- MegaBlog/src/appwrite/config.js
+++ MegaBlog/src/appwrite/config.js
@@ -92,6 +92,20 @@ export class Service{
 
     }
 
+    async uploadFile(file){
+        try{
+            return await this.bucket.createFile(
+                conf.appwriteBucketId,
+                ID.unique(),
+                file
+
+            )
+        }catch(error){
+            console.log("appwrite service:: upload file", error)
+            return false
+        }
+    }
+
 }
 
 const service = new Service()
