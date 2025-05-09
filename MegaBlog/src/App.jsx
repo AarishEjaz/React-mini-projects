ab1f17ee233788d2717933906a03e01e0d88c0fc
Aarish Ejaz <75199171+AarishEjaz@users.noreply.github.com>
2025-05-09 23:21:23 +0530

    added code

--- MegaBlog/src/App.jsx
+++ MegaBlog/src/App.jsx
@@ -1,5 +1,6 @@
 import { useState } from 'react'
 import{useDispatch} from 'react-redux'
+import authService from "./appwrite/auth"
 
 import './App.css'
 
@@ -8,6 +9,10 @@ function App() {
   const[loading,setLoading] = useState(true)
   const dispatch = useDispatch()
   console.log(import.meta.env.VITE_APPWRITE_URL)
+
+  useEffect(()=>{
+    authService
+  },[])
 
   return (
     <>
